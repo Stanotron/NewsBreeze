549c43e5ea41e737ac0f2d08dfc81136b118f0e6
Stanotron <jaintushar8864@gmail.com>
2022-09-19 22:32:55 +0530

    recycler unit

--- app/src/main/java/com/example/newsb/MainActivity.kt
+++ app/src/main/java/com/example/newsb/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.newsb
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val view: View = findViewById(android.R.id.content)
+        val mLoadAnimation: Animation = AnimationUtils.loadAnimation(applicationContext, R.drawable.fade_in)
+        mLoadAnimation.duration = 500
+        view.startAnimation(mLoadAnimation)
+
     }
 }
